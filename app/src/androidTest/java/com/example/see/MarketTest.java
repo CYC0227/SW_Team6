a0bca9bcc353b27ba2ec517d3dfcd865adff9e00
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2021-05-21 17:39:46 +0900

    test

---
+++ app/src/androidTest/java/com/example/see/MarketTest.java
@@ -0,0 +1,20 @@
+package com.example.see;
+
+import junit.framework.TestCase;
+
+import org.junit.Test;
+
+import javax.inject.Named;
+
+public class MarketTest extends TestCase {
+
+    @Test
+    public void test(){
+        assertEquals(4,4);
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(4,4);
+    }
+}
