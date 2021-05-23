e273d065cedbe804686de4e0188ad304879ed182
SoonHyung-Kwon <ccsa3321@naver.com>
2021-05-23 13:11:14 +0900

    prototype

---
+++ app/src/main/java/com/example/see/MarkerItem.java
@@ -0,0 +1,31 @@
+package com.example.see;
+
+public class MarkerItem {
+    int currentPeoples;
+    int maxPeoples;
+
+    public MarkerItem(int currentPeoples, int maxPeoples){
+        this.currentPeoples = currentPeoples;
+        this.maxPeoples = maxPeoples;
+    }
+
+    public MarkerItem(int currentPeoples){
+        this.currentPeoples = currentPeoples;
+    }
+
+    public void setCurrentPeoples(int currentPeoples){
+        this.currentPeoples = currentPeoples;
+    }
+
+    public int getCurrentPeoples(){
+        return currentPeoples;
+    }
+
+    public void setMaxPeoples(int maxPeoples){
+        this.maxPeoples = maxPeoples;
+    }
+
+    public int getMaxPeoples(){
+        return maxPeoples;
+    }
+}
